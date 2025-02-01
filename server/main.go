de1413e8ae96b3af31a112a97f1f7f430f27e8fa
Serafimchik <98609343+Serafimchik@users.noreply.github.com>
2025-02-01 21:10:03 +0300

    init

---
+++ server/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("Ошибка при обновлении соединения:", err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Ошибка при чтении сообщения:", err)
+			break
+		}
+
+		fmt.Printf("Получено сообщение: %s\n", message)
+
+		err = conn.WriteMessage(websocket.TextMessage, message)
+		if err != nil {
+			log.Println("Ошибка при отправке сообщения:", err)
+			break
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", handleWebSocket)
+
+	fmt.Println("Сервер запущен на :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("Ошибка при запуске сервера:", err)
+	}
+}
