8c7f521ee8719ade03043dcf2b028113731a550b
Serafimchik <98609343+Serafimchik@users.noreply.github.com>
2025-05-21 23:17:28 +0300

    fix cors 1

--- server/internal/appMiddleware/cors.go
+++ server/internal/appMiddleware/cors.go
@@ -15,8 +15,10 @@ func CorsMiddleware(next http.Handler) http.Handler {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
 		}
-		if r.Method == "OPTIONS" {
+
+		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
