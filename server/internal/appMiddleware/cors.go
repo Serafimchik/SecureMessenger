e5504fa547f2f246e59ba89e71052ff1279df06d
Serafimchik <98609343+Serafimchik@users.noreply.github.com>
2025-05-21 23:38:49 +0300

    fix cors 1

--- server/internal/appMiddleware/cors.go
+++ server/internal/appMiddleware/cors.go
@@ -1,19 +1,28 @@
 package appMiddleware
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
 
-		allowedOrigins := map[string]bool{
-			"http://localhost:3000":     true,
-			"http://31.41.155.240":      true,
-			"http://31.41.155.240:80":   true,
-			"http://31.41.155.240:8080": true,
+		allowedHosts := []string{
+			"31.41.155.240",
+			"localhost",
 		}
 
-		if allowedOrigins[origin] {
+		originAllowed := false
+		for _, host := range allowedHosts {
+			if strings.Contains(origin, host) {
+				originAllowed = true
+				break
+			}
+		}
+
+		if originAllowed {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
